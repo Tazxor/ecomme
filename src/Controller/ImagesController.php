0e86f9607d21e82abace0d6196f06c81cd296f30
Tazxor <79080127+Tazxor@users.noreply.github.com>
2024-05-13 11:17:09 +0200

    6 commit
    image crud acces admin

--- src/Controller/ImagesController.php
+++ src/Controller/ImagesController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/images')]
+#[Route('/image')]
 class ImagesController extends AbstractController
 {
     #[Route('/', name: 'app_images_index', methods: ['GET'])]
